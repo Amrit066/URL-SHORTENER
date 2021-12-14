ff31ddbc13f8b6cb3bd2a50d706d903f8cf9dce8
unknown <staining607@gmail.com>
2021-12-15 00:17:36 +0530

    Installed initial dependencies and created url-shortener component

---
+++ url-shortener/src/Url-Shortener-Components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Header} from './Header'
+import {Footer} from './Footer'
+
+
+export const Homepage = () => {
+    return (
+        
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
