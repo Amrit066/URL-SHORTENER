8b12189e8a9900bf88ab1f6bc1338199b24b550e
unknown <staining607@gmail.com>
2021-12-15 18:15:12 +0530

    Added Header, Section and Footer components

--- url-shortener/src/Url-Shortener-Components/Homepage.js
+++ url-shortener/src/Url-Shortener-Components/Homepage.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Header} from './Header'
+import {Section} from './Section'
 import {Footer} from './Footer'
 
 
 export const Homepage = () => {
     return (
         
-        <>
-        <Header />
-        <Footer />
-        </>
+        <div>
+            <Header /> 
+            <Section />  
+            <Footer />    
+        </div>
     )
 }
